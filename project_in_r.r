163a855a754631eb2e94f1b40d3e9d5371e6db78
Krzysztof Sikora <ksikora@platige.com>
2022-07-28 01:39:05 +0200

    R: fixed global vars

--- project_in_r.r
+++ project_in_r.r
@@ -55,50 +55,46 @@ for (year in years) {
 
 
 
-question_1 <- function() {
-  # Question 1. When is the best time to fly to minimise delays?
-  thresholds <- c(-1300, -90, -15, 15, 60, 120, 180, 300, 600, 1200, 1800)
-  # 1.1 time of day - for later
-  # 1.2 day of week
-  names_of_days_of_week = c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-  q1_2_data <- hash()
-  # loop through all years and get summaries for each year
-  for (year in years) {
-    # year as a string
-    str_year <- as.character(year)
-    # take data for the year
-    yearly_data_frame <- data_years[[str_year]]
-    yearly_data_frame <- as.data.frame(yearly_data_frame)
-    q1_2_data[str_year] <- yearly_data_frame %>% 
-      select(Year, DayOfWeek, ArrDelay) %>%   # narrow data down to just 3 columns
-      drop_na() %>%                           # drop N/A values
-      mutate(day_of_week = names_of_days_of_week[DayOfWeek]) # %>%   # add new column with name of day of week
-    
-    # create daily normalized histograms, i.e. with total area of bars = 1 
-    for (day in names_of_days_of_week) {
-      daily_delays <- q1_2_data[[str_year]] %>% 
-        filter(day_of_week == day)
-      just_delays <- daily_delays$ArrDelay
-      hist(just_delays,   # Store output of hist function
-           main = paste('Arrival delays per day of week', str_year, day, sep = ', '),
-           xlab = 'delays in minutes',
-           xlim = c(-200,200),
-           ylab = 'normalized frequency',
-           col = 'blue',
-           border = 'purple',
-           breaks = 500,  # unlist(thresholds))
-           freq = FALSE)
-    }
-    
-    # get data in the same intervals as in Python
-    q1_2_data[str_year] <- q1_2_data[[str_year]] %>% 
-      mutate(arr_delay_interval = cut(ArrDelay, thresholds)) %>%   # new column - delays in intervals
-      count(arr_delay_interval) %>%    # count number of different intervals
-      mutate(proc = n/sum(n)*100)      # find percentages
+# Question 1. When is the best time to fly to minimise delays?
+thresholds <- c(-1300, -90, -15, 15, 60, 120, 180, 300, 600, 1200, 1800)
+# 1.1 time of day - for later
+# 1.2 day of week
+names_of_days_of_week = c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+q1_2_data <- hash()
+# loop through all years and get summaries for each year
+for (year in years) {
+  # year as a string
+  str_year <- as.character(year)
+  # take data for the year
+  yearly_data_frame <- data_years[[str_year]]
+  yearly_data_frame <- as.data.frame(yearly_data_frame)
+  q1_2_data[str_year] <- yearly_data_frame %>% 
+    select(Year, DayOfWeek, ArrDelay) %>%   # narrow data down to just 3 columns
+    drop_na() %>%                           # drop N/A values
+    mutate(day_of_week = names_of_days_of_week[DayOfWeek]) # %>%   # add new column with name of day of week
+  
+  # create daily normalized histograms, i.e. with total area of bars = 1 
+  for (day in names_of_days_of_week) {
+    daily_delays <- q1_2_data[[str_year]] %>% 
+      filter(day_of_week == day)
+    just_delays <- daily_delays$ArrDelay
+    hist(just_delays,   # Store output of hist function
+         main = paste('Arrival delays per day of week', str_year, day, sep = ', '),
+         xlab = 'delays in minutes',
+         xlim = c(-200,200),
+         ylab = 'normalized frequency',
+         col = 'blue',
+         border = 'purple',
+         breaks = 500,  # unlist(thresholds))
+         freq = FALSE)
   }
+  
+  # get data in the same intervals as in Python
+  q1_2_data[str_year] <- q1_2_data[[str_year]] %>% 
+    mutate(arr_delay_interval = cut(ArrDelay, thresholds)) %>%   # new column - delays in intervals
+    count(arr_delay_interval) %>%    # count number of different intervals
+    mutate(proc = n/sum(n)*100)      # find percentages
 }
-
-question_1()
 
 
 
@@ -117,55 +113,51 @@ plane_data_data_frame <- plane_data_data_frame %>%
   filter(substrRight(issue_date, 4) >= year ) %>% 
   mutate(plane_date = as.Date(issue_date, "%m/%d/%Y"))
 
-question_2 <- function() {
-  q2_plane_data_df <- plane_data_data_frame %>% 
-    select(tailnum, plane_date) %>% 
-    drop_na()
-  
-  q2_data <- hash()
-  # loop through all years and get summaries for each year
-  for (year in 2007:2007) {
-    # year as a string
-    str_year <- as.character(year)
-    
-    # take data for the year
-    yearly_data_frame <- data_years[[str_year]]
-    
-    as.data.frame(yearly_data_frame)
-    
-    q2_data[str_year] <- yearly_data_frame %>% 
-      filter(Cancelled == 0) %>%                               # filter flights that were not cancelled
-      select(Year, Month, DayofMonth, TailNum, DepDelay) %>%   # narrow data down to 5 columns
-      drop_na() %>%                                            # drop N/A values
-      mutate(flight_date = as.Date(paste(as.character(Year), '/',# adds a column with date in proper format
-                                         as.character(Month), '/',
-                                         as.character(DayofMonth),
-                                         sep = ''),
-                                   "%Y/%m/%d"
-      ))
-    
-    q2_data_frame <- merge(x = q2_data[[str_year]], 
-                           y = q2_plane_data_df,
-                           by.x = "TailNum",
-                           by.y = 'tailnum') %>% 
-      mutate(age_of_plane = as.integer(difftime(flight_date, plane_date, units = 'days'))) %>%    # add new column with age of plane
-      select(age_of_plane, DepDelay)
-    
-    x <- q2_data_frame$age_of_plane
-    y <- q2_data_frame$DepDelay
-    # Plot with main and axis titles
-    # Change point shape (pch = 19) and remove frame.
-    plot(x, y, main = "age of plane vs delay",
-         xlab = "age of plane in days", ylab = "delay in minutes",
-         pch = 19, frame = FALSE)
-    # Add regression line
-    # abline(lm(y ~ x, data = q2_data_frame), col = "blue")
-    r_coef <- cor(x, y, method = "pearson", use = "complete.obs")
-    print(r_coef)
-  }
-}
+q2_plane_data_df <- plane_data_data_frame %>% 
+  select(tailnum, plane_date) %>% 
+  drop_na()
 
-question_2()
+q2_data <- hash()
+# loop through all years and get summaries for each year
+for (year in 2007:2007) {
+  # year as a string
+  str_year <- as.character(year)
+  
+  # take data for the year
+  yearly_data_frame <- data_years[[str_year]]
+  
+  as.data.frame(yearly_data_frame)
+  
+  q2_data[str_year] <- yearly_data_frame %>% 
+    filter(Cancelled == 0) %>%                               # filter flights that were not cancelled
+    select(Year, Month, DayofMonth, TailNum, DepDelay) %>%   # narrow data down to 5 columns
+    drop_na() %>%                                            # drop N/A values
+    mutate(flight_date = as.Date(paste(as.character(Year), '/',# adds a column with date in proper format
+                                       as.character(Month), '/',
+                                       as.character(DayofMonth),
+                                       sep = ''),
+                                 "%Y/%m/%d"
+    ))
+  
+  q2_data_frame <- merge(x = q2_data[[str_year]], 
+                         y = q2_plane_data_df,
+                         by.x = "TailNum",
+                         by.y = 'tailnum') %>% 
+    mutate(age_of_plane = as.integer(difftime(flight_date, plane_date, units = 'days'))) %>%    # add new column with age of plane
+    select(age_of_plane, DepDelay)
+  
+  x <- q2_data_frame$age_of_plane
+  y <- q2_data_frame$DepDelay
+  # Plot with main and axis titles
+  # Change point shape (pch = 19) and remove frame.
+  plot(x, y, main = "age of plane vs delay",
+       xlab = "age of plane in days", ylab = "delay in minutes",
+       pch = 19, frame = FALSE)
+  # Add regression line
+  # abline(lm(y ~ x, data = q2_data_frame), col = "blue")
+  r_coef <- cor(x, y, method = "pearson", use = "complete.obs")
+  print(r_coef)
+}
 
 
 
@@ -327,22 +319,17 @@ make_q3_scatter_plot <- function(plot_data, y_column) {
 } 
 
 
-question_3 <- function(q3_data) {
-  
-  q3_data <- get_q3_data(1988, 2007)
-  
-  make_q3_scatter_plot(q3_data, 'number_of_flights')
-  q3_data <- q3_data %>% 
-    filter(number_of_passengers > 0)
-  make_q3_scatter_plot(q3_data, 'number_of_passengers')
-  # year, get number of flights, total number of passengers, number of flights without known capacity
-  unknown_capacities <- q3_data %>% 
-    mutate(percentage_unknown = number_of_flights_without_a_record / number_of_flights * 100) %>% 
-    select(year, percentage_unknown)
-  View(unknown_capacities)
-}
+q3_data <- get_q3_data(1988, 2007)
 
-question_3(q3_data)
+make_q3_scatter_plot(q3_data, 'number_of_flights')
+q3_data <- q3_data %>% 
+  filter(number_of_passengers > 0)
+make_q3_scatter_plot(q3_data, 'number_of_passengers')
+# year, get number of flights, total number of passengers, number of flights without known capacity
+unknown_capacities <- q3_data %>% 
+  mutate(percentage_unknown = number_of_flights_without_a_record / number_of_flights * 100) %>% 
+  select(year, percentage_unknown)
+View(unknown_capacities)
 
 
 
